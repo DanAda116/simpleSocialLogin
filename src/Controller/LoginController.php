14844a37f913e41f003b754fe0b4983e2ccb73df
DanAda116 <daniel.adamczyk116@gmail.com>
2018-11-06 18:12:43 +0100

    Added simple login system via Google Account

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,18 +2,62 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
-class LoginController extends AbstractController
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Services\LoginService;
+
+class LoginController extends Controller
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(LoginService $login)
     {
+
+        $info = [];
+
+        if(isset($_SESSION['token'])) {
+            $client = $login->getClient();
+            $info = $login->getClientInfo($client);
+        }
+
+
         return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+            'data' => $info
         ]);
+    }
+
+    /**
+     *@Route("/google/", name="google")
+    */
+    public function googleLogin(LoginService $login)
+    {
+        $url = $login->getAuthUrl();
+
+        return $this->redirect($url);
+    }
+
+    /**
+     *@Route("/auth", name="auth")
+     */
+    public function googleAuth(LoginService $login)
+    {
+        $login->getClient();
+
+        return $this->render('login/loader.html.twig', [
+            'host' => $_SERVER['HTTP_HOST']
+        ]);
+    }
+
+    /**
+     *@Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+        if(isset($_SESSION))
+            session_destroy();
+
+        return $this->redirectToRoute('home');
     }
 }
---
+++ src/Services/LoginService.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Services;
+
+use Google_Client;
+
+class LoginService
+{
+    /**
+     * @var Google_Client $google_client
+     */
+    private $google_client;
+    private $projectDir;
+
+    /**
+     * LoginService constructor.
+     * @param Google_Client $google_client
+     * @param $projectDir
+     * @throws \Google_Exception
+     */
+
+    public function __construct(Google_Client $google_client, $projectDir)
+    {
+            $this->projectDir = $projectDir;
+
+            $this->google_client = $google_client;
+            $this->google_client->setAuthConfig($this->projectDir. '/client.json');
+            $this->google_client->setAccessType("offline");
+            $this->google_client->addScope('profile');
+            $this->google_client->setRedirectUri('http://localhost:8000/auth');
+    }
+
+
+    public function getAuthUrl() {
+        $auth_url = $this->google_client->createAuthUrl();
+        return filter_var($auth_url, FILTER_SANITIZE_URL);
+    }
+    public function getClient() {
+
+        if(!isset($_SESSION))
+            session_start();
+
+        if(isset($_GET['code'])) {
+
+            $token = $this->google_client->fetchAccessTokenWithAuthCode($_GET['code']);
+
+            $this->google_client->setAccessToken($token);
+
+            if ($this->google_client->isAccessTokenExpired()) {
+                echo 'expired';
+                if ($this->google_client->getRefreshToken()) {
+                    $this->google_client->fetchAccessTokenWithRefreshToken($this->google_client->getRefreshToken());
+                }
+            } else
+                echo 'ok';
+
+        } else
+            $this->google_client->setAccessToken($_SESSION['token']);
+
+        $_SESSION['token'] = $this->google_client->getAccessToken();
+
+        return $this->google_client;
+    }
+
+    public function getClientInfo(Google_Client $client){
+        $infoService = new \Google_Service_Plus($client);
+        $info = $infoService->people->get('me');
+
+        $data = [
+            'fullname' => $info->getDisplayName(),
+            'avatar_img' => $info->getImage()->url
+        ];
+
+
+        return $data;
+    }
+}
